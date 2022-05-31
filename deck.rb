53d53827e50b6aae0cbee185c89fe7be57ca284d
Robby Ackerley <rcackerley@me.com>
2022-05-31 18:17:28 -0400

    Initialize deck with 52 cards

--- deck.rb
+++ deck.rb
@@ -1,10 +1,13 @@
 require './card.rb'
 
 class Deck
-  # a deck has 4 suits: Clubs Diamonds Hearts Spades
-  # each suit has 13 ranks: 2 3 4 5 6 7 8 9 10, J, Q, K, A
-
+  
   def initialize
+    @cards = []
+    @discarded = []
+
+    @cards = SUITS.map{ |suit| cards_for(suit)}
+                  .flatten
   end
 
   def draw
@@ -17,5 +20,14 @@ class Deck
 
   def cheat
     # see the next card, but don't discard it
+  end
+
+  private 
+
+  SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades'].freeze
+  RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].freeze
+
+  def cards_for(suit)
+    RANKS.map{ |rank| Card.new(rank, suit)}
   end
 end
